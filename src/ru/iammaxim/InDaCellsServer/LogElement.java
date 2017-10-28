9da1cc2fb953be903b5c0154305ea175f014f878
Maettaog <geimerchannel@gmail.com>
2017-10-29 00:44:37 +0300

    Исправлен logElement

--- src/ru/iammaxim/InDaCellsServer/LogElement.java
+++ src/ru/iammaxim/InDaCellsServer/LogElement.java
@@ -14,28 +14,24 @@ public class LogElement {
 
     public String message;
     public String nick;
-    public int targetID;
 
-    public LogElement(Type type, String message, String nick, int targetID) {
+    public LogElement(Type type, String message, String nick) {
         this.type = type;
         this.message = message;
         this.nick = nick;
-        this.targetID = targetID;
     }
 
     public void write(DataOutputStream dos) throws IOException {
         dos.writeInt(type.ordinal());
         dos.writeUTF(message);
         dos.writeUTF(nick);
-        dos.writeInt(targetID);
     }
 
     public static LogElement read(DataInputStream dis) throws IOException {
         return new LogElement(
                 Type.values()[dis.readInt()],
                 dis.readUTF(),
-                dis.readUTF(),
-                dis.readInt()
+                dis.readUTF()
         );
     }
 }
