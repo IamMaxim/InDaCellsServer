ce5cea576d0402487c36e867daa04ac0ae50c130
Alexandr <naplle58@gmail.com>
2017-10-28 14:46:23 +0300

    Items

--- src/ru/iammaxim/InDaCellsServer/Items/Item.java
+++ src/ru/iammaxim/InDaCellsServer/Items/Item.java
@@ -3,30 +3,83 @@ package ru.iammaxim.InDaCellsServer.Items;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.util.HashMap;
 
 public class Item {
+    public static HashMap<Integer, Item> items = new HashMap<>();
+    public static HashMap<Integer, ItemWeapon> weapons = new HashMap<>();
+    public static HashMap<Integer, ItemArmor> armors = new HashMap<>();
 
     public final int id;
     public final String name;
     public final Type type;
+    public final float weight;
 
-    public Item(int id, Type type, String name) {
+    public static void registerItem(Item item){
+        items.put(item.id, item);
+    }
+
+    public static void registerItems(){
+        registerItem(new Item(0, Type.MISC, "Доска", 2));
+        registerItem(new Item(0, Type.MISC, "Железный прут", 2));
+        registerItem(new Item(0, Type.MISC, "Железная пластина", 3));
+        registerItem(new Item(0, Type.MISC, "Скотч", 0.5f));
+        registerItem(new Item(0, Type.MISC, "Гвозди", 0.5f));
+        registerItem(new Item(0, Type.MISC, "Верёвка", 1));
+    }
+
+    public static void registerWeapon(ItemWeapon weapon){
+        weapons.put(weapon.id, weapon);
+    }
+
+    public static void registerWeapons(){
+        registerWeapon(new ItemWeapon(0, Type.WEAPON, "Дрын", 1, 3));
+        registerWeapon(new ItemWeapon(1, Type.WEAPON, "Дубина с гвоздями", 2, 4));
+        registerWeapon(new ItemWeapon(2, Type.WEAPON, "Лук", 2, 3));
+        registerWeapon(new ItemWeapon(3, Type.WEAPON, "Полицейская дубина", 2, 1).addStat(ItemWeapon.AdditionalStats.ACCURACY, 5));
+        registerWeapon(new ItemWeapon(4, Type.WEAPON, "Полицейский пистолет", 3, 2).addStat(ItemWeapon.AdditionalStats.ACCURACY, 5));
+        registerWeapon(new ItemWeapon(5, Type.WEAPON, "Полицейское ружьё", 8, 8).addStat(ItemWeapon.AdditionalStats.ACCURACY, 5));
+        registerWeapon(new ItemWeapon(6, Type.WEAPON, "М14", 4, 5));
+        registerWeapon(new ItemWeapon(7, Type.WEAPON, "Пистолет из мусора", 2, 3));
+        registerWeapon(new ItemWeapon(8, Type.WEAPON, "Охотночье ружьё с мусора", 3, 6));
+        registerWeapon(new ItemWeapon(9, Type.WEAPON, "Кухонный нож", 1, 1));
+        registerWeapon(new ItemWeapon(10, Type.WEAPON, "Боевой нож", 2, 2));
+        registerWeapon(new ItemWeapon(11, Type.WEAPON, "Меч из музея", 3, 2));
+    }
+
+    public static void registerArmor(ItemArmor armor){
+        armors.put(armor.id, armor);
+    }
+
+    public static void registerArmors() {
+        registerArmor(new ItemArmor(0, "Тяжёлая мусорная броня", 12, 6));
+        registerArmor(new ItemArmor(1, "Тяжёлая полицейская броня", 20, 10));
+        registerArmor(new ItemArmor(2, "Лёгкая мусорная броня", 5, 3));
+        registerArmor(new ItemArmor(3, "Лёгкая полицейская броня", 8, 5));
+        registerArmor(new ItemArmor(4, "Тряпочная мусорная броня", 2, 1));
+        registerArmor(new ItemArmor(5, "Тряпочная полицейская броня", 3, 2));
+    }
+
+    public Item(int id, Type type, String name, float weight) {
         this.id = id;
         this.type = type;
         this.name = name;
+        this.weight = weight;
     }
 
     public void write(DataOutputStream dos) throws IOException {
         dos.writeInt(id);
         dos.writeUTF(name);
         dos.writeInt(type.ordinal());
+        dos.writeFloat(weight);
     }
 
     public static Item read(DataInputStream dis) throws IOException {
         Item item = new Item(
                 dis.readInt(),
                 Type.values()[dis.readInt()],
-                dis.readUTF()
+                dis.readUTF(),
+                dis.readFloat()
         );
 
         return item;
@@ -38,4 +91,5 @@ public class Item {
         FOOD,
         MISC
     }
+
 }
--- src/ru/iammaxim/InDaCellsServer/Items/ItemWeapon.java
+++ src/ru/iammaxim/InDaCellsServer/Items/ItemWeapon.java
@@ -1,7 +1,27 @@
 package ru.iammaxim.InDaCellsServer.Items;
 
+import java.util.HashMap;
+
 public class ItemWeapon extends Item {
-    public ItemWeapon(int id, Type type, String name) {
-        super(id, type, name);
+    private float damage;
+    private HashMap<AdditionalStats, Float> stats = new HashMap<>();
+
+    public ItemWeapon(int id, Type type, String name, float damage, float weight) {
+        super(id, Type.ARMOR, name, weight);
+        this.damage = damage;
+    }
+
+    public ItemWeapon addStat(AdditionalStats stat, float value){
+        stats.put(AdditionalStats.ACCURACY, value);
+
+        return this;
+    }
+
+    public float getDamage() {
+        return damage;
+    }
+
+    public enum AdditionalStats{
+        ACCURACY
     }
 }
