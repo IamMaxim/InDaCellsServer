ab8d8eafd28bb51ff6967de9e2187f58b9b6fd82
Maettaog <geimerchannel@gmail.com>
2017-10-29 07:18:50 +0300

    Исправлен packetSendMessage

--- src/ru/iammaxim/InDaCellsServer/Packets/PacketSendMessage.java
+++ src/ru/iammaxim/InDaCellsServer/Packets/PacketSendMessage.java
@@ -1,20 +1,29 @@
 package ru.iammaxim.InDaCellsServer.Packets;
 
-import ru.iammaxim.NetLib.Packet;
-
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 
+import ru.iammaxim.NetLib.Packet;
+
 public class PacketSendMessage extends Packet {
     public String message;
+
+    public PacketSendMessage() {
+        message = "message";
+    }
+
+    public PacketSendMessage(String message){
+        this.message = message;
+    }
+
     @Override
     public void write(DataOutputStream dos) throws IOException {
-
+        dos.writeUTF(message);
     }
 
     @Override
     public void read(DataInputStream dis) throws IOException {
-
+        message = dis.readUTF();
     }
 }
